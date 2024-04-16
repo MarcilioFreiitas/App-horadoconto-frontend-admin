b5f8d50137f383f909eb9a1f94b92c313326ab4a
Marcilio Freitas <mvf1@discente.ifpe.edu.br>
2024-04-16 18:06:20 -0300

    Corrigido bug na página de login da administração

--- lib/views/admin_login_screen.dart
+++ lib/views/admin_login_screen.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/service/auth_service_login.dart';
+import 'package:flutter_application_1/views/home_admin.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
 
-class AdminLoginScreen extends StatelessWidget {
+class AdminLoginScreen extends StatefulWidget {
+  @override
+  _AdminLoginScreenState createState() => _AdminLoginScreenState();
+}
+
+class _AdminLoginScreenState extends State<AdminLoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  String? errorMessage;
+
+  Future<void> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.0.106:8080/auth/loginadmin'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{'email': email, 'senha': password}),
+    );
+
+    if (response.statusCode == 200) {
+      print('Login bem-sucedido');
+      Map<String, dynamic> responseBody = jsonDecode(response.body);
+      String token = responseBody['token'];
+      print('Token: $token');
+
+      final storage = new FlutterSecureStorage();
+      await storage.write(key: 'token', value: token);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeAdmin()),
+      );
+    } else {
+      setState(() {
+        errorMessage = 'Falha ao fazer login. Verifique seu email e senha.';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +70,15 @@ class AdminLoginScreen extends StatelessWidget {
               onPressed: () {
                 String email = emailController.text;
                 String password = passwordController.text;
-                login(email, password, context);
+                login(email, password);
               },
               child: Text('Login'),
-            )
+            ),
+            if (errorMessage != null)
+              Text(
+                errorMessage!,
+                style: TextStyle(color: Colors.red),
+              ),
           ],
         ),
       ),
